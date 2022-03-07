5c39b346abe053e1aab065bfc49dda66089ab4e2
Gabriela Patricia Rodriguez <gprodriguez33@gmail.com>
2022-03-07 17:52:50 -0300

    metodo map en objetos

--- metodosArray.js
+++ metodosArray.js
@@ -24,6 +24,8 @@ const products = [{
 
 products.forEach(obj => console.log(obj.title + " $" + obj.price))
 
+
+
 //Metodo map (transforma - es inmutable) con for:
 
 const fruits = ["banana", "apple", "strawberry"];
@@ -44,3 +46,56 @@ console.log("nuevo", newArray);
 const newArray2 = fruits.map(item => item + "++");
 
 console.log("nuevo 2", newArray2);
+
+//El metodo map en un array de objetos
+
+const orders = [
+    {
+        name: "Patricia Quiroga",
+        Total: 340,
+        delivered: true,
+    },
+    {
+        name: "Lorena Samit",
+        Total: 560,
+        delivered: false, 
+    },
+    {
+        name: "Maximiliano Ayit",
+        Total: 1200,
+        delivered: true, 
+    },
+    {
+        name: "Maiten Rossi",
+        Total: 5600,
+        delivered: false, 
+    }
+]
+
+console.log("original", orders);
+const totales = orders.map(item => item.Total);
+console.log("totales", totales);
+
+//Que pasa si quiero agregar el item tax a mi nuevo array, sin modificar el array original
+
+// const rta2 = orders.map(item => {
+//     item.tax = .19;
+//     return item
+// });
+
+// console.log("original", orders);
+// console.log("rta2", rta2);
+
+//Solo pasa con los objetos, no con los valores primitivos
+//Estamos copiando la referencia en memoria
+
+const rta3 = orders.map(item => {
+    return {
+        ...item,
+        tax: .19
+    }
+}); //De esta forma, utilizando el script operation, no copiamos la referencia en memoria
+//y el array original permanece igual
+
+console.log("original", orders);
+console.log("rta3", rta3);
